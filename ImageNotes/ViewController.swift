1c02cc5828bdda1e93b6d94c2f662d6d282e49f4
Mohru <mohru-mac@Mohrus-Mac.local>
2016-12-30 01:03:12 +0100

    Initial Commit

---
+++ ImageNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageNotes
+//
+//  Created by Mohru on 12/30/16.
+//  Copyright © 2016 Mohru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
