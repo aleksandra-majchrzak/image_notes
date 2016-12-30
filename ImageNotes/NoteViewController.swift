83b62efd3705d78fe36d5e304a0f420d0d67c219
Mohru <mohru-mac@Mohrus-Mac.local>
2016-12-30 11:25:45 +0100

    Initial commit with working master-detail layout.

---
+++ ImageNotes/NoteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NoteViewController.swift
+//  ImageNotes
+//
+//  Created by Mohru on 12/30/16.
+//  Copyright © 2016 Mohru. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
